fb308d6bb5db94aca37315169b90fa52a351f8a7
millazs12 <millaray.zepeda@alumnos.ucn.cl>
2026-05-02 18:37:03 -0400

    PrimerComit

---
+++ src/logica2/Main.java
@@ -0,0 +1,12 @@
+package logica2;
+//Millaray Belen Zepeda Salfate| ICCI | 22.063.994-0
+//Vicente Ignacio Rojas Malhue | ICCI| 21.894.251-2
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
