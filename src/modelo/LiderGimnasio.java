386b63cd51f634349dbd1ab1248a6e4156155d7a
VicenteRojasMalhue <vicente.rojas03@alumnos.ucn.cl>
2026-05-07 15:56:11 -0400

    Corrección de conflictos y limpieza de modelo

--- src/modelo/LiderGimnasio.java
+++ src/modelo/LiderGimnasio.java
@@ -1,6 +1,5 @@
 package modelo;
 
-<<<<<<< HEAD
 import java.util.ArrayList;
 
 public class LiderGimnasio {
@@ -30,32 +29,7 @@ public class LiderGimnasio {
 	 }
 	 public void añadirEquipo(Pokemon p) {
 		 equipoGym.add(new Pokemon(p.getNombre(), p.getHabitat(), p.getPorcentajeAparicion(), p.getStats(), p.getTipo()));
-=======
 
-public class Gimnasio {
-	private int numeroGym;
-	 private String liderGym;
-	 private String estadoGym;
-	 private Pokemon[] pokemonesGym;
-	 
-	 public Gimnasio(int numeroGym, String liderGym, String estadoGym, Pokemon[] pokemonesGym) {
-		this.numeroGym = numeroGym;
-		this.liderGym = liderGym;
-		this.estadoGym = estadoGym;
-		this.pokemonesGym = pokemonesGym;
-	 }
-	 public int getNumeroGym() {
-		 return numeroGym;
-	 }
-	 public String getLiderGym() {
-		 return liderGym;
-	 }
-	 public String getEstadoGym() {
-		 return estadoGym;
-	 }
-	 public Pokemon[] getPokemones() {
-		 return pokemonesGym;
->>>>>>> c923e69df08a176a3e2e72e7364ece35f97a4003
 	 }
 
 }
